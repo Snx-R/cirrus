29b9066fbbe1574191bd7220e27d35a598e4a374
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-09-01 11:56:13 +0600

    initial commit
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -6,6 +6,9 @@ git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/l
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
 source build/envsetup.sh
+cd vendor/xiaomi/lavender
+git switch ten
+cd -
 cd device/xiaomi/lavender
 bash setup-makesfiles.sh
 cd -
