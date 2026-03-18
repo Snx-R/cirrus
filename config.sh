27992858eea59f416eda6f918df6aa450ede4a05
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2026-03-18 22:11:08 +0600

    artow

--- config.sh
+++ config.sh
@@ -1,17 +1,12 @@
-# add repo, dt, vt, kt etc.
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
-# replace with your manifest
-git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests
-# sync script
-repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
+repo init -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1_ext
+git clone https://github.com/Sa-Sajjad/manifest.git -b arrow-13 .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+cd kernel/xiaomi/lavender
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
+cd -
 
 source build/envsetup.sh
-export USE_CCACHE=1
-ccache -M 50G
-ccache -s
-export USE_GAPPS=true
-lunch nad_lavender-user
-make installclean
-mka nad
-export RELEASE=no
+lunch arrow_lavender-user
+m bacon
 Snx-R
