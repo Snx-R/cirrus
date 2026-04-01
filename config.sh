156280377debd0bd93db390ccc7fc9805e98aa89
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2026-04-01 10:19:34 +0600

    fahhhh
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@ repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ArrowOS-T/a
 git clone https://github.com/Sa-Sajjad/manifest.git -b arrow-13 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-cd kernel/xiaomi/lavender
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v3.1.0-legacy
-cd -
+# cd kernel/xiaomi/lavender
+# curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v3.1.0-legacy
+# cd -
 
 wget https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb && sudo dpkg -i libtinfo5_6.3-2_amd64.deb
 wget https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.3-2_amd64.deb && sudo dpkg -i libncurses5_6.3-2_amd64.deb
