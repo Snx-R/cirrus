08dd89f3708b18642ecb1e4506398a8837712da9
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2026-05-07 17:48:41 +0600

    fahh
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -3,19 +3,14 @@ repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusant
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
-source build/envsetup.sh
 git clone https://github.com/Sa-Sajjad/android_device_xiaomi_violet device/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_vendor_xiaomi_violet vendor/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_kernel_xiaomi_violet kernel/xiaomi/violet
-export TZ=Asia/Dhaka
-# export ALLOW_MISSING_DEPENDENCIES=true
 
+source build/envsetup.sh
+export TZ=Asia/Dhaka
 lunch nad_violet-user
 make installclean
-# export SELINUX_IGNORE_NEVERALLOWS=true
-# make api-stubs-docs || echo no problem
-# make system-api-stubs-docs || echo no problem
-# make test-api-stubs-docs || echo no problem
 mka nad
 export RELEASE=no
 Snx-R
