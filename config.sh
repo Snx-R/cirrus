cf5786e6b009d49edaa7c9c0535f1f3eaa96704b
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2026-05-07 11:29:00 +0600

    violet: Bringup for NusantarOS
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -1,13 +1,12 @@
 # add repo, dt, vt, kt etc.
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusantara-SiXtY-N9/android_manifest_nusa.git -b 10
+# sync script
+repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
+source build/envsetup.sh
 git clone https://github.com/Sa-Sajjad/android_device_xiaomi_violet device/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_vendor_xiaomi_violet vendor/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_kernel_xiaomi_violet kernel/xiaomi/violet
-
-# sync script
-repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
-source build/envsetup.sh
 export TZ=Asia/Dhaka
 # export ALLOW_MISSING_DEPENDENCIES=true
 
