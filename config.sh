2c741efb3d23ed59d502ed05b769a875b5da5681
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2026-05-07 11:07:49 +0600

    violet: Bringup for NusantarOS
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -4,12 +4,12 @@ repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/Nusant
 git clone https://github.com/Sa-Sajjad/android_device_xiaomi_violet device/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_vendor_xiaomi_violet vendor/xiaomi/violet
 git clone https://github.com/ResurrectionRemix-Devices/android_kernel_xiaomi_violet kernel/xiaomi/violet
-rm -rf hardware/xiaomi; git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-17.1 hardware/xiaomi
 
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 export TZ=Asia/Dhaka
+export ALLOW_MISSING_DEPENDENCIES=true
 
 lunch nad_lavender-user
 make installclean
